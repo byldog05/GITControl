d4a419918905f374255c53fc60a1bf70450b4912
Vsevolod Ban <vsevolod@Vsevolods-MacBook-Pro.local>
2017-10-10 20:51:42 +0300

    Added Image, Added Comment

--- GITControl/ViewController.swift
+++ GITControl/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageOutlet: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Hello comment
         // Do any additional setup after loading the view, typically from a nib.
     }
 
