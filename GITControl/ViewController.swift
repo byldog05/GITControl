7944cc4e0a818c7ea4b0f8d43f1cb2aa0e8b1d59
Vsevolod Ban <vsevolod@Vsevolods-MacBook-Pro.local>
2017-10-10 21:03:53 +0300

    Initial

--- GITControl/ViewController.swift
+++ GITControl/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Hello comment
         //Changes
+        //Changes
         // Do any additional setup after loading the view, typically from a nib.
     }
 
